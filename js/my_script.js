2d998b49235e4e27d26267c155cf71d9a9b4b435
therealmensumit <sk03950@gmail.com>
2022-05-08 15:22:36 +0530

    make 4 sections

--- js/my_script.js
+++ js/my_script.js
@@ -7,4 +7,46 @@ $(document).ready(function(){
     //     autoplay: true,
     //     autoplaySpeed: 2000,
     // });
+    var typed = new Typed('.typed-text', {
+        strings: ["Meet us at Odisha Jewellery Show "],
+        typeSpeed: 100,
+        backSpeed: 50,
+        delay: 10,
+        loop: true
+    });
+
+    $('.client-slider').slick({
+        dots: false,
+        infinite: false,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        speed: 300,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: false
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
 });
