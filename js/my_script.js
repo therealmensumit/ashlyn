08cb674fa899e5bdf0b96ffbc402af4db023b720
therealmensumit <93430783+therealmensumit@users.noreply.github.com>
2022-05-08 09:25:43 +0530

    first-commit

---
+++ js/my_script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    // $('.main-slide').slick({
+    //     dots: true,
+    //     arrows: true,
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //     autoplay: true,
+    //     autoplaySpeed: 2000,
+    // });
+});
